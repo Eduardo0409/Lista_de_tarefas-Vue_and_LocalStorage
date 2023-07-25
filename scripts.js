eb75687e48df0d528927407fa16b07fd6c70319e
Eduardo0409 <du.caldas.alves@gmail.com>
2023-07-25 14:00:07 -0300

    Projeto finalizado

---
+++ scripts.js
@@ -0,0 +1,35 @@
+
+const todosApp = {
+    data() {
+        return {
+            todos: [],
+            newTodo: {
+                done: false
+            }
+        }
+    },
+
+    methods: {
+        addTodo: function(){
+            if (this.newTodo.text){
+                this.todos.push(this.newTodo);
+                this.newTodo = {
+                    done: false
+                };
+                localStorage.setItem("todos", JSON.stringify(this.todos));
+            } else {
+                alert("O texto da tarefa tem que ser preenchido!")
+            }
+        }
+    },
+
+    created(){
+        this.todos = localStorage.getItem("todos") ? JSON.parse(localStorage.getItem("todos")) : this.todos;
+    },
+    updated(){
+        localStorage.setItem("todos", JSON.stringify(this.todos));
+        console.log("updated")
+    }
+}
+
+Vue.createApp(todosApp).mount('#app');
